8de6938d672036de6de1ec318641b474ed039d1f
TranThinh <105481775+TranThinh2203@users.noreply.github.com>
2025-12-10 20:46:47 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>Thông tin cá nhân</h1>
+      <p>Họ và tên: Trần Gia Phúc Thịnh</p>
+      <p>Tuổi: 22</p>
+      <p>Nghề nghiệp: Nhân viên văn phòng</p>
+      <p>Sở thích: Xe phân khối lớn, đọc truyện tranh, xem phim, võ thuật, chơi game</p>
+    </div>
+  );
+}
+
+export default App;
